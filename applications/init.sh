28f9b0cae70a467442235697d35169442e279dc6
lgaliana <lino.galiana@insee.fr>
2025-02-21 15:28:46 +0000

    bourrin

---
+++ applications/init.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Define the configuration directory for VS Code
+VSCODE_CONFIG_DIR="$HOME/.local/share/code-server/User"
+
+# Create the configuration directory if necessary
+mkdir -p "$VSCODE_CONFIG_DIR"
+
+# User settings file
+SETTINGS_FILE="$VSCODE_CONFIG_DIR/settings.json"
+
+
+code-server --install-extension yzhang.markdown-all-in-one
+code-server --install-extension oderwat.indent-rainbow
+code-server --install-extension tamasfe.even-better-toml
+code-server --install-extension aaron-bond.better-comments
+code-server --install-extension github.vscode-github-actions
+
+
+# Replace default flake8 linter with project-preconfigured ruff
+code-server --uninstall-extension ms-python.flake8
+code-server --install-extension charliermarsh.ruff
+
+
+jq '. + {
+    "workbench.colorTheme": "Default Dark Modern",  # Set the theme
+
+    "editor.rulers": [80, 100, 120],  # Add specific vertical rulers
+    "files.trimTrailingWhitespace": true,  # Automatically trim trailing whitespace
+    "files.insertFinalNewline": true,  # Ensure files end with a newline
+
+    "flake8.args": [
+        "--max-line-length=100"  # Max line length for Python linting
+    ]
+
+
+
+}' "$SETTINGS_FILE" > "$SETTINGS_FILE.tmp" && mv "$SETTINGS_FILE.tmp" "$SETTINGS_FILE"
